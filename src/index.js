554f544c10893f82402fa13113764f8d13e0c9b4
Caroline <carolinelopesrdg@hotmail.com>
2024-06-12 13:50:03 -0300

    alterando rotas

--- src/index.js
+++ src/index.js
@@ -1,36 +1,75 @@
 const express = require('express');
+const { v4: uuid4 } = require('uuid');
+
 const app = express();
 app.use(express.json())
 
-app.get('/projects', function(request, response){
-    return response.json({
-        message: 'Óla Dev'
-    })
+const projects = [];
+
+app.get('/projects', function (request, response) {
+    return response.json(projects)
 });
 
 // GET http://localhost:8080/projects?title=Node&owner=Carol&page=1
 
-app.get('/projects', function(request, response){
+app.get('/projects', function (request, response) {
     const { title, owner, page } = request.query
-    console.log(title, owner, page)
-
-    return response.json([
-        'Projeto 1',
-        'projeto 2'
-    ])
+    return response.json(projects)
 });
 
-app.put('/projects/:id', function(request, response){
+app.post('/projects', function (request, response) {
+    const { name, owner } = request.body;
+
+    const project = {
+        id: uuid4,
+        name,
+        owner
+    };
+
+    projects.push(project);
+
+    return response.status(201).json(project);
+})
+
+app.put('/projects/:id', function (request, response) {
     const { id } = request.params
-    console.log(id)
+    const { name, owner } = request.body;
 
-    return response.json([
-        'Projeto 1',
-        'projeto 2',
-        'projeto 3',
-    ])
+    const projectIndex = projects.findIndex(p => p.id == id)
+
+    if (projectIndex < 0) {
+        return response.status(404).json({ error: 'Project nao encontrado' })
+    }
+
+    if (!name || !owner) {
+        return response.status(404).json({ error: 'Title e Owner sao requiridos' })
+    }
+
+    const project = {
+        id,
+        name,
+        owner
+    };
+
+    projects[projectIndex] = project
+
+    return response.status(200).json(project)
 });
 
+app.delete('/projects/:id', function (request, response) {
+    const { id } = request.params
+
+    const projectIndex = projects.findIndex(p => p.id == id);
+
+    if (projectIndex < 0) {
+        return response.status(404).json({ error: 'Project nao encontrado' })
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return response.status(204).send;
+
+})
 app.listen(8080, () => {
     console.log('Server started on port 8080!')
 });
